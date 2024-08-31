9567f45fa23ef9c00a48faf4b2191776b7da1d23
NO ONE <sohaibikram249@gmail.com>
2024-08-31 07:27:27 +0500

    update

--- webScraping.py
+++ webScraping.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 
 page_to_scrape = requests.get("https://quotes.toscrape.com/")
-# page_to_scrape = requests.get("https://www.scrapethissite.com/pages/")
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 quotes = soup.findAll("span", attrs={"class": "text"})
 authors = soup.findAll("small", attrs={"class": "author"})
@@ -11,8 +10,7 @@ prices = soup.findAll("p", attrs={"class": "lead session-desc"})
 
 for quote, author in zip(quotes, authors):
     print(quote.text + "\n\t" + author.text)
-# for price in prices:
-#     print(price.text.lstrip())
+
 
 
 
